9fa22e5200ce3a9e244d2087cf2296c30148d1a9
3nprob <3nprob@3nprob>
2021-10-08 03:10:06 +0900

    mp4 gallery comments

--- src/util.ts
+++ src/util.ts
@@ -3,8 +3,8 @@ export const proxyURL = (url: string): string =>
 
 export const linkify = (content: string) =>
   content.replace(
-    /https?:\/\/[^.]*\.imgur.com\/([\/_a-zA-Z0-9-]+)\.gifv/g,
-    '<video src="/$1.mp4" class="commentVideo commentObject" loop="" autoplay=""></video>'
+    /https?:\/\/[^.]*\.imgur.com\/([\/_a-zA-Z0-9-]+)\.(gifv|mp4)/g,
+    '<video src="/$1.mp4" class="commentVideo commentObject" loop="" autoplay="" controls=""></video>'
   ).replace(
     /https?:\/\/[^.]*\.imgur.com\/([\/_a-zA-Z0-9-]+\.[a-z0-9A-Z]{2,6})/g,
     '<a href="/$1" target="_blank"><img class="commentImage commentObject" src="/$1" loading="lazy" /></a>'
