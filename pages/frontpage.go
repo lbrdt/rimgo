55e96df4dff4517bac2b1d57f7320d669a362b0b
video-prize-ranch <cb.8a3w5@simplelogin.co>
2022-01-28 19:07:44 -0500

    Fix CSP

--- pages/frontpage.go
+++ pages/frontpage.go
@@ -8,7 +8,7 @@ import (
 func FrontpageHandler(c *fiber.Ctx) error {
 	utils.SetHeaders(c)
 	c.Set("Cache-Control", "public,max-age=31557600")
-	c.Set("Content-Security-Policy", "default-src 'none'; style-src 'self' *.cloudfront.net; img-src 'self' *.cloudfront.net; font-src 'self'; block-all-mixed-content")
+	c.Set("Content-Security-Policy", "default-src 'none'; style-src 'self' *.cloudfront.net; img-src 'self' *.cloudfront.net; font-src 'self' *.cloudfront.net; block-all-mixed-content")
 
 	return c.Render("frontpage", fiber.Map{})
 }
