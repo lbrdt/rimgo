55969985118491bb9bede16674e22ce787774ec8
video-prize-ranch <cb.8a3w5@simplelogin.co>
2022-02-02 10:20:14 -0500

    Allow fullscreen in permissions-policy

--- utils/setHeaders.go
+++ utils/setHeaders.go
@@ -8,5 +8,5 @@ func SetHeaders(c *fiber.Ctx) {
 	c.Set("X-Content-Type-Options", "nosniff")
 	c.Set("X-Robots-Tag", "noindex, noimageindex, nofollow")
 	c.Set("Strict-Transport-Security", "max-age=31557600")
-	c.Set("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()")
+	c.Set("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(self), geolocation=(), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()")
 }
