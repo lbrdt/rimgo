41012edf9ed71c3a38fa9fe9f1cc72ac01d32939
video-prize-ranch <cb.8a3w5@simplelogin.co>
2022-02-02 10:22:48 -0500

    Add pages for users

--- pages/user.go
+++ pages/user.go
@@ -1,6 +1,7 @@
 package pages
 
 import (
+	"strconv"
 	"sync"
 
 	"codeberg.org/video-prize-ranch/rimgo/api"
@@ -13,6 +14,16 @@ func HandleUser(c *fiber.Ctx) error {
 	utils.SetHeaders(c)
 	c.Set("Cache-Control", "public,max-age=604800")
 	c.Set("Content-Security-Policy", "default-src 'none'; media-src 'self'; style-src 'unsafe-inline' 'self'; img-src 'self'; font-src 'self'; block-all-mixed-content")
+
+	page := "0"
+	if c.Query("page") != "" {
+		page = c.Query("page")
+	}
+
+	pageNumber, err := strconv.Atoi(c.Query("page"))
+	if err != nil {
+		pageNumber = 0
+	}
 
 	wg := sync.WaitGroup{}
 	wg.Add(2)
@@ -28,7 +39,7 @@ func HandleUser(c *fiber.Ctx) error {
 	submissions, err := []types.Submission{}, error(nil)
 	go func() {
 		defer wg.Done()
-		submissions, err = api.FetchSubmissions(c.Params("userID"), "newest", "0")
+		submissions, err = api.FetchSubmissions(c.Params("userID"), "newest", page)
 	}()
 	if err != nil {
 		return err
@@ -38,5 +49,8 @@ func HandleUser(c *fiber.Ctx) error {
 	return c.Render("user", fiber.Map{
 		"user":        user,
 		"submissions": submissions,
+		"page":        page,
+		"nextPage": 	 pageNumber + 1,
+		"prevPage": 	 pageNumber - 1,
 	})
 }
